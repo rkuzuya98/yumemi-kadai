baa1df5f02db07fe354bffe2c86f3f06ab6f5f96
rkuzuya <riku.kuzuya@gmail.com>
2021-05-18 11:13:23 +0900

    人口データ取得関数作成

---
+++ src/async/getPopulationData.js
@@ -0,0 +1,29 @@
+import { highchatsAPI } from "../highcharts";
+
+export const getPopulationData = (
+  selectingPrefectures,
+  setPopulations
+) => {
+  let altPopulations = []
+  async () => {
+    await Promise.all(
+      selectingPrefectures.map(async(item) => {
+        await fetch(
+          `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${item}`,
+          {
+            headers: { "X-API-KEY": highchatsAPI },
+          }
+        )
+          .then((response) => response.json())
+    
+          .then((res) => {
+            altPopulations.push({
+              // name: getJpnPrefecture(item),
+              name: item,
+              data: res.result.data[0].data.map((item) => item.value)})
+          });
+      })
+      )
+      setPopulations(altPopulations)
+  }
+}
